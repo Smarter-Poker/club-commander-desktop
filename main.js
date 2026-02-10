af2aaa37dcfcdf2de2b9fe8bb432cde7f2678031
Smarter-Poker <admin@smarter.poker>
2026-02-09 22:28:23 -0600

    fix: open to registration page, not staff login

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let mainWindow;
 let tray;
 
 // Production URL
-const COMMANDER_URL = 'https://smarter.poker/commander/login';
+const COMMANDER_URL = 'https://smarter.poker/commander/register';
 
 // Auto-updater configuration
 autoUpdater.autoDownload = true;
